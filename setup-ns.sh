81fd813df353fc202005f7d634a39b8193b61d8a
Ishaan Singh <55971005+bigBrain1901@users.noreply.github.com>
2022-04-27 21:20:07 +0530

    Updated for Demo with TCL - Apr 28

--- setup-ns.sh
+++ setup-ns.sh
@@ -1,21 +1,32 @@
-#!/bin/sh
-
-ip netns add mptcp-client
-ip netns add mptcp-server
+ip netns add h1
+ip netns add h2
 
 sysctl -w net.ipv4.conf.all.rp_filter=0
-ip netns exec mptcp-client sysctl -w net.mptcp.enabled=1
-ip netns exec mptcp-server sysctl -w net.mptcp.enabled=1
+ip netns exec h1 sysctl -w net.mptcp.enabled=1
+ip netns exec h2 sysctl -w net.mptcp.enabled=1
 
-ip link add red-client netns mptcp-client type veth peer red-server netns mptcp-server
-ip link add blue-client netns mptcp-client type veth peer blue-server netns mptcp-server
+ip link add eth0a netns h1 type veth peer eth1a netns h2
+ip link add eth0b netns h1 type veth peer eth1b netns h2
 
-ip -n mptcp-server address add 10.0.0.1/24 dev red-server
-ip -n mptcp-server address add 192.168.0.1/24 dev blue-server
-ip -n mptcp-client address add 10.0.0.2/24 dev red-client
-ip -n mptcp-client address add 192.168.0.2/24 dev blue-client
+ip -n h1 address add 10.0.0.1/24 dev eth0a
+ip -n h1 address add 192.168.0.1/24 dev eth0b
+ip -n h2 address add 10.0.0.2/24 dev eth1a
+ip -n h2 address add 192.168.0.2/24 dev eth1b
 
-ip -n mptcp-server link set red-server up
-ip -n mptcp-server link set blue-server up
-ip -n mptcp-client link set red-client up
-ip -n mptcp-client link set blue-client up
+ip netns exec h1 tc qdisc add dev eth0a root netem delay 5ms rate 10mbit
+ip netns exec h1 tc qdisc add dev eth0b root netem delay 10ms rate 5mbit
+ip netns exec h2 tc qdisc add dev eth1a root netem delay 5ms rate 10mbit
+ip netns exec h2 tc qdisc add dev eth1b root netem delay 10ms rate 5mbit
+
+ip -n h1 link set eth0a up
+ip -n h1 link set eth0b up
+ip -n h2 link set eth1b up
+ip -n h2 link set eth1a up
+
+ip netns
+ip netns exec h1 ip link
+ip netns exec h2 ip link
+ip netns exec h1 tc qdisc show dev eth0a
+ip netns exec h1 tc qdisc show dev eth0b
+ip netns exec h2 tc qdisc show dev eth1a
+ip netns exec h2 tc qdisc show dev eth1b
